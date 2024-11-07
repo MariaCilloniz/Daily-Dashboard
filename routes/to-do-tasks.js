f79d4aa7fa2dcd4c6da1682cc221e35d8a0d21a5
Emily Schur <ejschur@gmail.com>
2024-11-07 10:00:51 -0800

    Add task endpoints

--- routes/to-do-tasks.js
+++ routes/to-do-tasks.js
@@ -1,14 +1,51 @@
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
+import { writeData, readData } from "../utils/dataUtils.js";
 
 const router = express.Router();
 
-// Get task list
+router.get("/", (req, res) => {
+  res.status(200).json(readData());
+});
 
-// Get task by id
+router.post("/", (req, res) => {
+  const { title, description, tags } = req.body;
 
-// Add task
+  const newTask = {
+    id: uuidv4(),
+    title,
+    description,
+    tags,
+  };
 
-// Delete task
+  const tasks = readData();
+  tasks.push(newTask);
+  writeData(tasks);
+
+  res.status(201).json(newTask);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const tasks = readData();
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+
+  if (taskIndex === -1) {
+    res
+      .status(404)
+      .json({ message: "No task with that id exists. Could not delete task." });
+  }
+
+  const deletedTask = tasks.splice(taskIndex, 1);
+  writeData(tasks);
+  res.status(200).json(deletedTask);
+});
+
+router.get("/:tag", (req, res) => {
+  const { tag } = req.params;
+  const tasks = readData();
+  const filteredTasks = tasks.filter((task) => task.tags.includes(tag));
+  res.status(200).json(filteredTasks);
+});
 
 export default router;
